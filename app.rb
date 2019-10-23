e0b472ca0ddf9a35c38b1850c04e380efe1758c6
Stephen Blais <stephen@s-blais.com>
2019-10-23 18:53:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
     params[:number].times do
       output += params[:phrase]
     end
-    output
+    "#{output}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
