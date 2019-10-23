f1b6e9f2cc753689015b2739376d9fb30765d4f1
Stephen Blais <stephen@s-blais.com>
2019-10-23 19:14:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,7 +24,6 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    #binding.pry
     case params[:operation]
     when "add"
       "#{params[:number1].to_i + params[:number2].to_i}"
