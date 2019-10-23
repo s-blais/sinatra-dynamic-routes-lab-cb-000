a2d332a914dd5fe0ef81417f27744e21fdbb058f
Stephen Blais <stephen@s-blais.com>
2019-10-23 19:13:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   get '/square/:number' do
-    (params[:number].to_i * params[:number].to_i)
+    "#{params[:number].to_i * params[:number].to_i}"
   end
 
   get '/say/:number/:phrase' do
